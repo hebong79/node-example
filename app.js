945369ae7c8b5db0e6e39e2ed6ed1b63b5ce50f6
hebong79 <hebong79@gmail.com>
2023-11-28 16:43:59 +0900

    문법 함수

---
+++ app.js
@@ -0,0 +1,11 @@
+"use srtict"
+
+const testVar = require('./testVariable.js');
+const testFun = require('./testFunc.js');
+const testArray = require('./testArray.js');
+
+testVar.Test_Var();
+//testVar.TestTypes();
+
+let value1 = testFun.multiply3(40, 50);
+console.log('mutiply = ' + value1);
